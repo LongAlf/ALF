6e05aa78112b0b76a8b10090d82e2a269d96994f
LongAlf <nefzaifan@gmail.com>
2025-06-14 07:54:01 +0800

    Update ITC.lua

--- ITC.lua
+++ ITC.lua
@@ -1,4 +1,5 @@
---if SC2 then
+--lua
+if SC2 then
 function TAPCLICK()
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ArmWrestleService"):WaitForChild("RF"):WaitForChild("RequestClick"):InvokeServer()
 game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ArmWrestleService"):WaitForChild("RF"):WaitForChild("RequestClick"):InvokeServer()
